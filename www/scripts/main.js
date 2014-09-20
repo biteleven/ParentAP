3443f5e4d34c5579a500c0f103278939f9de60e7
Biteleven <sigalas@bit11.gr>
2014-09-20 22:02:51 +0300

    x

---
+++ www/scripts/main.js
@@ -0,0 +1,215 @@
+
+// Wait for Apache Cordova to load
+document.addEventListener("deviceready", onDeviceReady, false);
+
+// PhoneGap is ready
+function onDeviceReady() {
+    
+	
+}
+
+var airlinesApp = function(){}
+
+airlinesApp.prototype = function() {
+    var _flightForCheckin = null,
+    _flightForDetails=null,
+    _ffNum = null, 
+    _customerData = null,
+    _login = false,
+    
+    run = function(){
+        var that = this,
+        $seatPicker=$('#seatPicker');
+        $('#tripDetail').on('pagebeforeshow',$.proxy(_initTripDetail,that));
+        $('#boardingPass').on('pageshow',$.proxy(_initBoardingPass,that));
+        $('#home').on('pagebeforecreate',$.proxy(_initHome,that));
+        $('#checkIn').on('pageshow', $.proxy(_initCheckIn,that));
+        
+        $('#myTripsListView').on('click', 'li', function () {
+        	var item = $(this);
+        	_flightForCheckin = item.data('flight');
+            _flightForDetails = item.data('flight');
+        });
+        
+        $seatPicker.on('pageshow', function (event) {
+        	var el = $('#seatMapPickerContainer', this),
+        	seat = _flightForCheckin.segments[_flightForCheckin.currentSegment].seat;
+        	seatMapDrawing.drawSeatMap(el, seat);
+        
+        });
+        
+        $seatPicker.on('pagebeforehide', function (event) {
+        	_flightForCheckin.segments[_flightForCheckin.currentSegment].seat = seatMapDrawing.getselectedSeat();
+        });
+    },
+    
+    _initTripDetail = function(){
+        var seg = _flightForDetails.segments[0];
+	    $('#tripDetail-title').text(seg.from + ' to ' + seg.to);
+	    $('#tripDetail-flightNum').text(seg.flightNum);
+	    $('#tripDetail-depart').text(seg.departDate + ' at ' + seg.time);
+	    $('#tripDetail-seat').text(seg.seat);
+	    seg = _flightForDetails.segments[1];
+	    $('#tripDetail-return-title').text(seg.from + ' to ' + seg.to);
+	    $('#tripDetail-return-flightNum').text(seg.flightNum);
+	    $('#tripDetail-return-depart').text(seg.departDate + ' at ' + seg.time);
+        $('#tripDetail-return-seat').text(seg.seat);
+    },
+    
+    _initBoardingPass = function(){
+        currentseg = _flightForCheckin.segments[_flightForCheckin.currentSegment];
+
+	    $('#boardingpass-cnum').text(_flightForCheckin.cNum);
+	    $('#boardingpass-passenger').text(_customerData.firstName + ' ' + _customerData.lastName);
+	    $('#boardingpass-seat').text(currentseg.seat);
+	    $('#boardingpass-gate').text(currentseg.gate);
+	    $('#boardingpass-depart').text(currentseg.time);
+	    var flight = currentseg.flightNum + ':' + currentseg.from + ' to ' + currentseg.to;
+	    $('#boardingpass-flight').text(flight);
+    },
+    
+    _initHome = function(){
+        if (!_login) {
+	    	$.mobile.changePage("#activity", { transition: "flip" });
+
+	    }
+    },
+    
+    _initCheckIn = function(){
+        var currentseg = _flightForCheckin.segments[_flightForCheckin.currentSegment],
+	    seat = currentseg.seat,
+	    flight = currentseg.from + ' to ' + currentseg.to;
+	    $('#checkIn-flight-number').text(currentseg.flightNum);
+	    $('#checkIn-flight-destination').text(flight);
+        
+	    $('#checkIn-seat').text(seat);
+    },
+    
+    _handleLogOn = function (ff, success) {
+		if (success) {
+			_ffNum = ff;
+			airData.getDataforFF(_ffNum,_handleDataForFF);
+		}
+	},
+    
+    _handleDataForFF = function (data) {
+        $flightList = $('#myTripsListView');
+		_customerData = data;
+		$('#ffname').text(data.firstName);
+		$('#ffnum').text(data.ffNum);
+		$('#currentStatus').text(data.status);
+		$('#miles').text(data.miles);
+		$('#numberOfFlights').text(data.flights.length);
+		for (var i in data.flights) {
+			var flight = data.flights[i],
+            currentSegment = flight.segments[flight.currentSegment];
+			$flightList.append('<li id="' + flight.id + '"><a href="#tripDetail" data-transition="slide">' + currentSegment.from + ' to ' + currentSegment.to + '</a></li>');
+			var item = $('#' + flight.id, $flightList);
+			item.data('flight', flight);
+			if (flight.timeToCheckIn) {
+
+				item.addClass('checkIn');
+				$('a', item).attr('href', '#checkIn');
+			}
+			else {
+				item.addClass('tripDetail');
+			}
+		}
+		$.mobile.changePage('#home', { transition: 'flip' });
+
+	};
+    
+    return {
+        run:run,
+    };
+}();
+
+// Emergency Call Functions
+// process the confirmation dialog result
+function onEmergencyConfirm(button) {
+    alert('Επιλέξατε ' + button);
+}
+
+// Show a custom confirmation dialog
+//
+function showEmergencyConfirm() {
+    navigator.notification.confirm(
+        'Επιβεβαίωση κλήσης ανάγκης',  // message
+        onEmergencyConfirm,              // callback to invoke with index of button pressed
+        'Κλήση ανάγκης',            // title
+        'Κλήση,Ακύρωση'          // buttonLabels
+    );
+}
+
+//Geolocation functions
+
+var watch_id = null;    // ID of the geolocation
+//var tracking_data = []; // Array containing GPS position objects
+
+function startGeolocation(){
+    console.log("Location search started!");
+    // Start tracking the User
+
+    initMap();
+    watch_id = navigator.geolocation.watchPosition(
+
+        // Success
+        function(position){
+            console.log("Location Updated");
+            $("#currentLocation").html('Latitude: '  + position.coords.latitude      + '<br />' +
+                                        'Longitude: ' + position.coords.longitude     + '<br />' +
+                                        '<hr />');
+            marker.setPosition( new google.maps.LatLng( position.coords.latitude , position.coords.longitude ) );
+            map.panTo( new google.maps.LatLng( position.coords.latitude,  position.coords.longitude  ) );
+        },
+
+        // Error
+        function(error){
+            console.log(error);
+            alert("GPS Error");
+        },
+
+        // Settings
+        { frequency: 3000, enableHighAccuracy: true });
+
+    $("#startTracking_status").html("Tracking Started");
+}
+
+function stopGeolocation(){
+    console.log("Location search stopped!");
+        // Stop tracking the user
+        navigator.geolocation.clearWatch(watch_id);
+
+        // Save the tracking data
+        //window.localStorage.setItem(track_id, JSON.stringify(tracking_data));
+
+        // Reset watch_id and tracking_data
+        var watch_id = null;
+        //var tracking_data = null;
+
+        $("#startTracking_status").html("Tracking Stopped");
+
+}
+
+var marker;
+var map;
+function initMap()
+{
+
+    // Set the initial Lat and Long of the Google Map
+    var myLatLng = new google.maps.LatLng(38.04393,23.804930);
+
+// Google Map options
+    var myOptions = {
+        zoom: 15,
+        center: myLatLng,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+
+// Create the Google Map, set options
+    map = new google.maps.Map( document.getElementById( 'map_canvas' ), myOptions );
+    marker = new google.maps.Marker( {position: myLatLng, map: map} );
+
+    marker.setMap( map );
+
+}
